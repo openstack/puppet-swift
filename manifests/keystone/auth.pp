87ab008565303cf813e4b21ad9da036a43fb144c
Joe Topjian <joe@topjian.net>
2012-10-28 13:28:35 -0600

    Region changes

--- manifests/keystone/auth.pp
+++ manifests/keystone/auth.pp
@@ -2,7 +2,8 @@ class swift::keystone::auth(
   $auth_name = 'swift',
   $password  = 'swift_password',
   $address   = '127.0.0.1',
-  $port      = '8080'
+  $port      = '8080',
+  $region    = 'RegionOne'
 ) {
 
   keystone_user { $auth_name:
@@ -20,9 +21,8 @@ class swift::keystone::auth(
     type        => 'object-store',
     description => 'Openstack Object-Store Service',
   }
-  keystone_endpoint { $auth_name:
+  keystone_endpoint { "${region}/${auth_name}":
     ensure       => present,
-    region       => 'RegionOne',
     public_url   => "http://${address}:${port}/v1/AUTH_%(tenant_id)s",
     admin_url    => "http://${address}:${port}/",
     internal_url => "http://${address}:${port}/v1/AUTH_%(tenant_id)s",
@@ -33,9 +33,8 @@ class swift::keystone::auth(
     type        => 's3',
     description => 'Openstack S3 Service',
   }
-  keystone_endpoint { "${auth_name}_s3":
+  keystone_endpoint { "${region}/${auth_name}_s3":
     ensure       => present,
-    region       => 'RegionOne',
     public_url   => "http://${address}:${port}",
     admin_url    => "http://${address}:${port}",
     internal_url => "http://${address}:${port}",
