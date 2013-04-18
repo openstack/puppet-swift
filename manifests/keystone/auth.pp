004b8b41488a9658763153a967a1a02bfaa5af8c
newptone <yuxcer@gmail.com>
2013-04-18 12:12:51 +0800

    Add address params to the swift::keystone::auth
    Fix bug 1170194
    
       This patch contains:
           * add param admin_address and internal_address
           * fix tab misusing
    
    Change-Id: Id3865240a3d5d289c557fd8cc51d6f1d77057ed8

--- manifests/keystone/auth.pp
+++ manifests/keystone/auth.pp
@@ -1,26 +1,41 @@
 class swift::keystone::auth(
-  $auth_name = 'swift',
-  $password  = 'swift_password',
-  $address   = '127.0.0.1',
-  $port      = '8080',
-  $tenant    = 'services',
-  $email     = 'swift@localhost',
-  $region    = 'RegionOne',
-  $public_protocol = 'http',
-  $public_address = undef,
-  $public_port = undef
+  $auth_name         = 'swift',
+  $password          = 'swift_password',
+  $address           = '127.0.0.1',
+  $port              = '8080',
+  $tenant            = 'services',
+  $email             = 'swift@localhost',
+  $region            = 'RegionOne',
+  $public_protocol   = 'http',
+  $public_address    = undef,
+  $public_port       = undef,
+  $admin_address     = undef,
+  $internal_address  = undef
 ) {
 
-  if ! $public_port {
-	$real_public_port = $port
-  } else {
-	$real_public_port = $public_port
+if $address != '127.0.0.1' {
+  warning("Address parameter for swift::keystone::auth has been deprecated, use public_address instead")
   }
 
-  if ! $public_address {
-	$real_public_address = $address
+  if ! $public_port {
+    $real_public_port = $port
   } else {
-	$real_public_address = $public_address
+    $real_public_port = $public_port
+  }
+  if ! $public_address {
+    $real_public_address = $address
+  } else {
+    $real_public_address = $public_address
+  }
+  if ! $admin_address {
+    $real_admin_address = $real_public_address
+  } else {
+    $real_admin_address = $admin_address
+  }
+  if ! $internal_address {
+    $real_internal_address = $real_public_address
+  } else {
+    $real_internal_address = $internal_address
   }
 
   keystone_user { $auth_name:
@@ -43,8 +58,8 @@ class swift::keystone::auth(
   keystone_endpoint { "${region}/${auth_name}":
     ensure       => present,
     public_url   => "${public_protocol}://${real_public_address}:${real_public_port}/v1/AUTH_%(tenant_id)s",
-    admin_url    => "http://${address}:${port}",
-    internal_url => "http://${address}:${port}/v1/AUTH_%(tenant_id)s",
+    admin_url    => "http://${real_admin_address}:${port}/",
+    internal_url => "http://${real_internal_address}:${port}/v1/AUTH_%(tenant_id)s",
   }
 
   keystone_service { "${auth_name}_s3":
@@ -55,8 +70,8 @@ class swift::keystone::auth(
   keystone_endpoint { "${region}/${auth_name}_s3":
     ensure       => present,
     public_url   => "${public_protocol}://${real_public_address}:${real_public_port}",
-    admin_url    => "http://${address}:${port}",
-    internal_url => "http://${address}:${port}",
+    admin_url    => "http://${real_admin_address}:${port}",
+    internal_url => "http://${real_internal_address}:${port}",
   }
 
 }
