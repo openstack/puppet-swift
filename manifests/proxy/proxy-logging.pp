0f7601a3587511b633d0a04ffa24f42f19b3786c
Mathieu Gagné <mgagne@iweb.com>
2013-08-28 20:19:14 -0400

    Deprecate classes with hyphens
    Puppet does not recommend the use of hyphens in class names.
    
    This change deprecates classes with hyphens and inform the users
    to use the new ones. Backward compatibility is preserved.
    
    Change-Id: I4991c956e297d76423a805558d9b6731473a4a51

--- manifests/proxy/proxy-logging.pp
+++ manifests/proxy/proxy-logging.pp
@@ -1,20 +1,5 @@
 #
-# Configure swift proxy-logging.
-#
-# == Dependencies
-#
-# == Examples
-#
-# == Authors
-#
-#   Joe Topjian joe@topjian.net
-#
 class swift::proxy::proxy-logging {
-
-  concat::fragment { 'swift_proxy-logging':
-    target  => '/etc/swift/proxy-server.conf',
-    content => template('swift/proxy/proxy-logging.conf.erb'),
-    order   => '27',
-  }
-
+  warning('swift::proxy::proxy-logging is deprecated. Use swift::proxy::proxy_logging instead.')
+  include swift::proxy::proxy_logging
 }
