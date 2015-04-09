4539c84521729e083db6dce052b915e1f6c59ebe
Emilien Macchi <emilien@redhat.com>
2015-04-09 15:23:01 -0400

    Notify services if swift.conf is modified
    swift.conf contains the hash_suffix. With the current module, we have a
    race condition where services can start *before* having the right hash
    in swift.conf, so some objects mays be put in quarantine.
    
    When usint swift_config (which affect swift.conf), we should restart all
    services to ensure our ring is running correctly.
    
    Change-Id: I1087ba4ec079ca9c43bea9ff0511347632871ec2
    Closes-bug: #1442329
    (cherry picked from commit 6dc85b0f7918fa295974f7afbb1a6b8a26f8f953)

--- manifests/storage/container.pp
+++ manifests/storage/container.pp
@@ -22,6 +22,10 @@ class swift::storage::container(
   $package_ensure     = 'present',
   $allowed_sync_hosts = ['127.0.0.1'],
 ) {
+
+  Swift_config<| |> ~> Service['swift-container-updater']
+  Swift_config<| |> ~> Service['swift-container-auditor']
+
   swift::storage::generic { 'container':
     manage_service => $manage_service,
     enabled        => $enabled,
@@ -70,5 +74,6 @@ class swift::storage::container(
       provider  => $::swift::params::service_provider,
       require   => File['/etc/init/swift-container-sync.conf', '/etc/init.d/swift-container-sync']
     }
+    Swift_config<| |> ~> Service['swift-container-sync']
   }
 }
