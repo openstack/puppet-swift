f34cd0865b337127e6184db8699016bc59906eee
Emilien Macchi <emilien@redhat.com>
2015-09-22 13:47:11 -0400

    Bump RDO packages to tested repos
    RDO has now [1] a repo that is tested against real CI.
    Using this repository will avoid more failures in our CI.
    
    [1] http://trunk.rdoproject.org/centos7-liberty/current-passed-ci/
    
    Change-Id: Ia45b1cc281838c8213cf91a5286316778de91a96

--- spec/acceptance/basic_swift_spec.rb
+++ spec/acceptance/basic_swift_spec.rb
@@ -33,7 +33,7 @@ describe 'basic swift' do
                 'gpgcheck' => 'no',
               },
               'openstack-liberty-trunk' => {
-                'baseurl'  => 'http://trunk.rdoproject.org/centos7-liberty/current/',
+                'baseurl'  => 'http://trunk.rdoproject.org/centos7-liberty/current-passed-ci/',
                 'descr'    => 'openstack-liberty-trunk',
                 'gpgcheck' => 'no',
               },
