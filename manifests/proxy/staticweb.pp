239a8e592cbda00ee1ea55606aedaa313c232713
Thiago da Silva <thiago@redhat.com>
2016-11-02 14:08:27 -0400

    set url_base option in staticweb swift middleware
    the url_base option was recently added upstream to resolve
    issue with SSL termination by haproxy.
    
    Closes-Bug: 1638632
    
    Change-Id: Icf45cf2aece398b836c87ddffde5d3056e96dc4d
    Signed-off-by: Thiago da Silva <thiago@redhat.com>

--- manifests/proxy/staticweb.pp
+++ manifests/proxy/staticweb.pp
@@ -8,6 +8,12 @@
 #
 #  include 'swift::proxy::staticweb'
 #
+# == Parameters
+#
+# [*url_base*]
+#   (optional) The URL scheme and/or the host name used to generate redirects.
+#   Defaults to $::os_service_default.
+#
 # == Authors
 #
 #   Mehdi Abaakouk <sileht@sileht.net>
@@ -16,11 +22,14 @@
 #
 # Copyright 2012 eNovance licensing@enovance.com
 #
-class swift::proxy::staticweb() {
+class swift::proxy::staticweb(
+  $url_base = $::os_service_default
+) {
 
   include ::swift::deps
 
   swift_proxy_config {
     'filter:staticweb/use':              value => 'egg:swift#staticweb';
+    'filter:staticweb/url_base':         value => $url_base;
   }
 }
