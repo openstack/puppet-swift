2976c7d889cf7bc29a61e4bbf3fba662f08c3840
Christian Schwede <cschwede@redhat.com>
2019-08-30 12:24:22 +0200

    Add support for audit middleware
    Co-Authored-By: Christian Schwede <cschwede@redhat.com>
    Depends-On: Id1b2944783ecaaa631ce9c1ceebfa1a30de4d4c6
    Change-Id: Ifa4e2ed894844ab1c7501f8edc18e55a9dea7e61

---
+++ manifests/proxy/audit.pp
@@ -0,0 +1,31 @@
+# == Class: swift::proxy::audit
+#
+# Configure audit middleware for Swift proxy.
+#
+# === Parameters
+#
+# [*filter_factory*]
+#   (Optional) The audit filter factory.
+#   Defaults to 'keystonemiddleware.audit:filter_factory'
+#
+# [*audit_map_file*]
+#   (Optional) The audit map file.
+#   Defaults to '/etc/pycadf/swift_api_audit_map.conf'
+#
+# == Authors
+#
+# Christian Schwede <cschwede@redhat.com>
+# Tobias Urdin <tobias.urdin@binero.se>
+#
+class swift::proxy::audit (
+  $filter_factory = 'keystonemiddleware.audit:filter_factory',
+  $audit_map_file = '/etc/pycadf/swift_api_audit_map.conf',
+) {
+
+  include ::swift::deps
+
+  swift_proxy_config {
+    'filter:audit/paste.filter_factory': value => $filter_factory;
+    'filter:audit/audit_map_file':       value => $audit_map_file;
+  }
+}
