8930de7b4f687b7efc6736eda1938f5ef2def7ea
Takashi Kajinami <tkajinam@redhat.com>
2020-07-16 00:29:38 +0900

    Add support for the interface parameter in authtoken middleware
    This patch adds support for [keystone_authtoken] interface parameter,
    so that operators can define which endpoint should be used by authtoken
    middleware.
    
    Change-Id: Iebb67609a384ee4c6be1d0f8aff86643ff6775c2
    (cherry picked from commit a09758194acee428f793abb975fdd92affcb2314)
    (cherry picked from commit 263240ea845b4cc96b8e82e6fa884005023fe78f)

--- manifests/proxy/authtoken.pp
+++ manifests/proxy/authtoken.pp
@@ -71,6 +71,11 @@
 #  true/false
 #  Defaults to $::os_service_default.
 #
+# [*interface*]
+#  (Optional) Interface to use for the Identity API endpoint. Valid values are
+#  "public", "internal" or "admin".
+#  Defaults to $::os_service_default.
+#
 # == DEPRECATED
 #
 # [*identity_uri*]
@@ -116,6 +121,7 @@ class swift::proxy::authtoken(
   $include_service_catalog      = false,
   $service_token_roles          = $::os_service_default,
   $service_token_roles_required = $::os_service_default,
+  $interface                    = $::os_service_default,
   # DEPRECATED PARAMETERS
   $admin_user                   = undef,
   $admin_tenant_name            = undef,
@@ -182,5 +188,6 @@ class swift::proxy::authtoken(
     'filter:authtoken/include_service_catalog':      value => $include_service_catalog;
     'filter:authtoken/service_token_roles':          value => $service_token_roles;
     'filter:authtoken/service_token_roles_required': value => $service_token_roles_required;
+    'filter:authtoken/interface':                    value => $interface,
   }
 }
