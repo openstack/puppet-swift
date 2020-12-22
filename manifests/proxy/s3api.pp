42d74471dd1f6f4104a3be0218f007566342bedd
Takashi Kajinami <tkajinam@redhat.com>
2020-12-22 23:26:52 +0900

    Deprecate ineffective swift::proxy::s3api::ensure
    The swift::proxy::s3api::ensure parameter is currently unused. Let's
    just deprecate the parameter because it is useless.
    
    Change-Id: I57463d8d6c6756793d397371648ce692dd2c2d09

--- manifests/proxy/s3api.pp
+++ manifests/proxy/s3api.pp
@@ -5,10 +5,6 @@
 #
 # == Parameters
 #
-# [*ensure*]
-#   Enable or not s3api middleware
-#   Defaults to 'present'
-#
 # [*auth_pipeline_check*]
 #   Enable pipeline order check
 #   Defaults to 'false'
@@ -17,16 +13,27 @@
 #   Max upload per num
 #   Default to 1000.
 #
+# DEPRECATED PARAMETERS
+#
+# [*ensure*]
+#   Enable or not s3api middleware
+#   Defaults to undef
+#
 class swift::proxy::s3api(
-  $ensure = 'present',
   $auth_pipeline_check = false,
   $max_upload_part_num = 1000,
+  # DEPRECATED PARAMETERS
+  $ensure              = undef,
 ) {
 
   include swift::deps
 
+  if $ensure != undef {
+    warning('The ensure parameter has been deprecated and has no effect')
+  }
+
   swift_proxy_config {
-    'filter:s3api/use': value => 'egg:swift#s3api';
+    'filter:s3api/use':                 value => 'egg:swift#s3api';
     'filter:s3api/auth_pipeline_check': value => $auth_pipeline_check;
     'filter:s3api/max_upload_part_num': value => $max_upload_part_num;
   }
