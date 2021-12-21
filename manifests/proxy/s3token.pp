b9f4681c1757a004bbb52fa531a998c6541532f5
Takashi Kajinami <tkajinam@redhat.com>
2021-12-21 09:19:15 +0900

    s3token: Remove deprecated auth_* parameters
    ... because these were deprecated several cycles ago[1].
    
    [1] bb93e6a3755b39cb2408d299ee3015ebed819b20
    
    Change-Id: I3ce2ceaf9181eaa11990310070209a30d8507457

--- manifests/proxy/s3token.pp
+++ manifests/proxy/s3token.pp
@@ -4,18 +4,6 @@
 #
 # === Parameters
 #
-# [*auth_host*]
-#   (optional) The keystone host
-#   Defaults to undef.
-#
-# [*auth_port*]
-#   (optional) The Keystone client API port
-#   Defaults to undef.
-#
-# [*auth_protocol*]
-#   (optional) http or https
-#    Defaults to undef.
-#
 # [*auth_uri*]
 #   (optional) The Keystone server uri
 #   Defaults to http://127.0.0.1:5000
@@ -90,9 +78,6 @@
 # Copyright 2012 eNovance licensing@enovance.com
 #
 class swift::proxy::s3token(
-  $auth_host             = undef,
-  $auth_port             = undef,
-  $auth_protocol         = undef,
   $auth_uri              = 'http://127.0.0.1:5000',
   $reseller_prefix       = 'AUTH_',
   $delay_auth_decision   = false,
@@ -109,13 +94,6 @@ class swift::proxy::s3token(
 
   include swift::deps
 
-  if $auth_host and $auth_port and $auth_protocol {
-    warning('Use of the auth_host, auth_port, and auth_protocol options have been deprecated in favor of auth_uri.')
-    $auth_uri_real = "${auth_protocol}://${auth_host}:${auth_port}"
-  } else {
-    $auth_uri_real = $auth_uri
-  }
-
   if $password == undef {
     warning('Usage of the default password is deprecated and will be removed in a future release. \
 Please set password parameter')
@@ -126,7 +104,7 @@ Please set password parameter')
 
   swift_proxy_config {
     'filter:s3token/use':                   value => 'egg:swift#s3token';
-    'filter:s3token/auth_uri':              value => $auth_uri_real;
+    'filter:s3token/auth_uri':              value => $auth_uri;
     'filter:s3token/reseller_prefix':       value => $reseller_prefix;
     'filter:s3token/delay_auth_decision':   value => $delay_auth_decision;
     'filter:s3token/http_timeout':          value => $http_timeout;
