openstack/puppet-swift

commit 152587fd2a1d0c0bc7ad67585fe2ecdd6abde995

Remove unnecessary group assignment
ceilometermiddleware does not require access to local resources owned
by the ceilometer group but communicate with ceilometer over rpc.
Thus we don't need to add the swift user to the ceilometer group.

Change-Id: I3666c5b3d45ba99a5716cfbe5174202b53ad4950

manifests/proxy/ceilometer.pp
@@ -29,12 +29,6 @@
 #   Enable or not ceilometer fragment
 #   Defaults to 'present'
 #
-# [*group*]
-#   Group name to add to 'swift' user.
-#   ceilometer/eventlet: set 'ceilometer' (default)
-#   ceilometer/wsgi: set $::apache::group
-#   Defaults to 'ceilometer'
-#
 # [*nonblocking_notify*]
 #   Whether to send events to messaging driver in a background thread
 #   Defaults to false
@@ -112,6 +106,10 @@
 #   (Optional) Complete public Identity API endpoint.
 #   Defaults to undef
 #
+# [*group*]
+#   Group name to add to 'swift' user.
+#   Defaults to undef
+#
 # == Examples
 #
 # == Authors
@@ -128,7 +126,6 @@ class swift::proxy::ceilometer(
   $topic                      = undef,
   $control_exchange           = undef,
   $ensure                     = 'present',
-  $group                      = 'ceilometer',
   $nonblocking_notify         = false,
   $ignore_projects            = ['services'],
   $auth_url                   = 'http://127.0.0.1:5000',
@@ -147,16 +144,11 @@ class swift::proxy::ceilometer(
   $rabbit_use_ssl             = $::os_service_default,
   $kombu_ssl_version          = $::os_service_default,
   # DEPRECATED PARAMETERS
-  $auth_uri                   = undef
+  $auth_uri                   = undef,
+  $group                      = undef,
 ) inherits swift {
 
   include swift::deps
-
-  User['swift'] {
-    groups +> $group,
-  }
-
-  Package<| tag == 'ceilometer-package' |> -> User['swift']
 
   if defined(Service['swift-proxy-server']) {
     Package['python-ceilometermiddleware'] -> Service['swift-proxy-server']
@@ -164,6 +156,10 @@ class swift::proxy::ceilometer(
 
   if $auth_uri {
     warning('The swift::proxy::ceilometer::auth_uri parameter was deprecated, and has no effect')
+  }
+
+  if $group != undef {
+    warning('The group parameer has been deprecated and has no effect now.')
   }
 
   if $password == undef {

spec/classes/swift_proxy_ceilometer_spec.rb
@@ -20,7 +20,6 @@ describe 'swift::proxy::ceilometer' do
       it { is_expected.to contain_swift_proxy_config('filter:ceilometer/paste.filter_factory').with_value('ceilometermiddleware.swift:filter_factory') }
       it { is_expected.to contain_swift_proxy_config('filter:ceilometer/url').with_value('rabbit://user_1:user_1_passw@1.1.1.1:5673/rabbit').with_secret(true) }
       it { is_expected.to contain_swift_proxy_config('filter:ceilometer/nonblocking_notify').with_value('false') }
-      it { is_expected.to contain_user('swift').with_groups('ceilometer') }
 
       it { is_expected.to contain_package('python-ceilometermiddleware').with(
         :ensure => 'present',
@@ -31,8 +30,7 @@ describe 'swift::proxy::ceilometer' do
 
     describe "when overriding default parameters with rabbit driver" do
       let :params do
-        { :group                 => 'www-data',
-          :default_transport_url => 'rabbit://user_1:user_1_passw@1.1.1.1:5673/rabbit',
+        { :default_transport_url => 'rabbit://user_1:user_1_passw@1.1.1.1:5673/rabbit',
           :driver                => 'messagingv2',
           :topic                 => 'notifications',
           :control_exchange      => 'swift',
@@ -50,7 +48,6 @@ describe 'swift::proxy::ceilometer' do
       end
 
       context 'with single rabbit host' do
-        it { is_expected.to contain_user('swift').with_groups('www-data') }
         it { is_expected.to contain_swift_proxy_config('filter:ceilometer/paste.filter_factory').with_value('ceilometermiddleware.swift:filter_factory') }
         it { is_expected.to contain_swift_proxy_config('filter:ceilometer/url').with_value('rabbit://user_1:user_1_passw@1.1.1.1:5673/rabbit').with_secret(true) }
         it { is_expected.to contain_swift_proxy_config('filter:ceilometer/driver').with_value('messagingv2') }

manifests/init.pp
@@ -65,11 +65,6 @@ class swift(
     tag    => ['openstack', 'swift-package'],
   }
 
-  user {'swift':
-    ensure  => present,
-    require => Anchor['swift::install::end'],
-  }
-
   swift_config {
     'swift-hash/swift_hash_path_suffix': value => $swift_hash_path_suffix;
     'swift-hash/swift_hash_path_prefix': value => $swift_hash_path_prefix;

releasenotes/notes/deprecate-ceilometer-group-81408169438202b6.yaml
@@ -1,0 +1,5 @@
+---
+deprecations:
+  - |
+    The ``swift::proxy::ceilometer::group`` parameter has been deprecated and
+    has no effect now.

spec/classes/swift_spec.rb
@@ -28,8 +28,6 @@ describe 'swift' do
         }
       end
 
-      it {is_expected.to contain_user('swift')}
-
       it 'configures swift.conf' do
         is_expected.to contain_swift_config(
           'swift-hash/swift_hash_path_suffix').with_value('string')

commit 6e834c34b35fb8c75168012d29ccd7d6f037db0f

Support more [oslo_messaging_rabbit] parameters
The ceilometer middleware uses these options when creating notification
client instances.

Conflicts:
	manifests/proxy/ceilometer.pp

Resolved conflicts caused by 152587fd2a1d0c0bc7ad67585fe2ecdd6abde995 .

Change-Id: I2a1d8e2d559bda4fda229f40aec7d97d14b10b11
(cherry picked from commit 67454cf7ff499c7c11b0a0ae8d1563dcc904f2ed)

manifests/proxy/ceilometer.pp
@@ -97,13 +97,64 @@
 #   Defaults to $::os_service_default.
 #
 # [*rabbit_use_ssl*]
-#   (optional) Boolean. Connect over SSL for RabbitMQ. (boolean value)
+#   (Optional) Connect over SSL for RabbitMQ. (boolean value)
 #   Defaults to $::os_service_default
 #
 # [*kombu_ssl_version*]
 #   (optional) SSL version to use (valid only if SSL enabled).
 #   Valid values are TLSv1, SSLv23 and SSLv3. SSLv2 may be
 #   available on some distributions. (string value)
+#   Defaults to $::os_service_default
+#
+# [*rabbit_ha_queues*]
+#   (Optional) Use HA queues in RabbitMQ (x-ha-policy: all). If you change this
+#   option, you must wipe the RabbitMQ database. (boolean value)
+#   Defaults to $::os_service_default
+#
+# [*rabbit_heartbeat_timeout_threshold*]
+#   (Optional) Number of seconds after which the Rabbit broker is
+#   considered down if heartbeat's keep-alive fails
+#   (0 disable the heartbeat). EXPERIMENTAL. (integer value)
+#   Defaults to $::os_service_default
+#
+# [*rabbit_heartbeat_rate*]
+#   (Optional) How often times during the heartbeat_timeout_threshold
+#   we check the heartbeat. (integer value)
+#   Defaults to $::os_service_default
+#
+# [*rabbit_heartbeat_in_pthread*]
+#   (Optional) EXPERIMENTAL: Run the health check heartbeat thread
+#   through a native python thread. By default if this
+#   option isn't provided the  health check heartbeat will
+#   inherit the execution model from the parent process. By
+#   example if the parent process have monkey patched the
+#   stdlib by using eventlet/greenlet then the heartbeat
+#   will be run through a green thread.
+#   Defaults to $::os_service_default
+#
+# [*rabbit_qos_prefetch_count*]
+#   (Optional) Specifies the number of messages to prefetch.
+#   Defaults to $::os_service_default
+#
+# [*amqp_durable_queues*]
+#   (optional) Define queues as "durable" to rabbitmq.
+#   Defaults to $::os_service_default
+#
+# [*kombu_reconnect_delay*]
+#   (Optional) How long to wait before reconnecting in response
+#   to an AMQP consumer cancel notification. (floating point value)
+#   Defaults to $::os_service_default
+#
+# [*kombu_failover_strategy*]
+#   (Optional) Determines how the next RabbitMQ node is chosen in case the one
+#   we are currently connected to becomes unavailable. Takes effect only if
+#   more than one RabbitMQ node is provided in config. (string value)
+#   Defaults to $::os_service_default
+#
+# [*kombu_compression*]
+#   (optional) Possible values are: gzip, bz2. If not set compression will not
+#   be used. This option may notbe available in future versions. EXPERIMENTAL.
+#   (string value)
 #   Defaults to $::os_service_default
 #
 # == DEPRECATED
@@ -123,31 +174,40 @@
 # Copyright 2013 eNovance licensing@enovance.com
 #
 class swift::proxy::ceilometer(
-  $default_transport_url      = undef,
-  $driver                     = $::os_service_default,
-  $topic                      = undef,
-  $control_exchange           = undef,
-  $ensure                     = 'present',
-  $group                      = 'ceilometer',
-  $nonblocking_notify         = false,
-  $ignore_projects            = ['services'],
-  $auth_url                   = 'http://127.0.0.1:5000',
-  $auth_type                  = 'password',
-  $project_name               = 'services',
-  $project_domain_name        = 'Default',
-  $system_scope               = $::os_service_default,
-  $username                   = 'swift',
-  $user_domain_name           = 'Default',
-  $password                   = undef,
-  $region_name                = $::os_service_default,
-  $notification_ssl_ca_file   = $::os_service_default,
-  $notification_ssl_cert_file = $::os_service_default,
-  $notification_ssl_key_file  = $::os_service_default,
-  $amqp_ssl_key_password      = $::os_service_default,
-  $rabbit_use_ssl             = $::os_service_default,
-  $kombu_ssl_version          = $::os_service_default,
+  $default_transport_url              = undef,
+  $driver                             = $::os_service_default,
+  $topic                              = undef,
+  $control_exchange                   = undef,
+  $ensure                             = 'present',
+  $group                              = 'ceilometer',
+  $nonblocking_notify                 = false,
+  $ignore_projects                    = ['services'],
+  $auth_url                           = 'http://127.0.0.1:5000',
+  $auth_type                          = 'password',
+  $project_name                       = 'services',
+  $project_domain_name                = 'Default',
+  $system_scope                       = $::os_service_default,
+  $username                           = 'swift',
+  $user_domain_name                   = 'Default',
+  $password                           = undef,
+  $region_name                        = $::os_service_default,
+  $notification_ssl_ca_file           = $::os_service_default,
+  $notification_ssl_cert_file         = $::os_service_default,
+  $notification_ssl_key_file          = $::os_service_default,
+  $amqp_ssl_key_password              = $::os_service_default,
+  $rabbit_use_ssl                     = $::os_service_default,
+  $kombu_ssl_version                  = $::os_service_default,
+  $rabbit_ha_queues                   = $::os_service_default,
+  $rabbit_heartbeat_timeout_threshold = $::os_service_default,
+  $rabbit_heartbeat_rate              = $::os_service_default,
+  $rabbit_heartbeat_in_pthread        = $::os_service_default,
+  $rabbit_qos_prefetch_count          = $::os_service_default,
+  $amqp_durable_queues                = $::os_service_default,
+  $kombu_reconnect_delay              = $::os_service_default,
+  $kombu_failover_strategy            = $::os_service_default,
+  $kombu_compression                  = $::os_service_default,
   # DEPRECATED PARAMETERS
-  $auth_uri                   = undef
+  $auth_uri                           = undef,
 ) inherits swift {
 
   include swift::deps
@@ -202,15 +262,27 @@ Please set password parameter')
   }
 
   if $default_transport_url =~ /^rabbit.*/ {
-    oslo::messaging::rabbit {'swift_proxy_config':
-      kombu_ssl_ca_certs => $notification_ssl_ca_file,
-      kombu_ssl_certfile => $notification_ssl_cert_file,
-      kombu_ssl_keyfile  => $notification_ssl_key_file,
-      kombu_ssl_version  => $kombu_ssl_version,
-      rabbit_use_ssl     => $rabbit_use_ssl,
+    oslo::messaging::rabbit { 'swift_proxy_config':
+      rabbit_ha_queues            => $rabbit_ha_queues,
+      heartbeat_timeout_threshold => $rabbit_heartbeat_timeout_threshold,
+      heartbeat_rate              => $rabbit_heartbeat_rate,
+      heartbeat_in_pthread        => $rabbit_heartbeat_in_pthread,
+      rabbit_qos_prefetch_count   => $rabbit_qos_prefetch_count,
+      amqp_durable_queues         => $amqp_durable_queues,
+      kombu_ssl_ca_certs          => $notification_ssl_ca_file,
+      kombu_ssl_certfile          => $notification_ssl_cert_file,
+      kombu_ssl_keyfile           => $notification_ssl_key_file,
+      kombu_ssl_version           => $kombu_ssl_version,
+      rabbit_use_ssl              => $rabbit_use_ssl,
+      kombu_reconnect_delay       => $kombu_reconnect_delay,
+      kombu_failover_strategy     => $kombu_failover_strategy,
+      kombu_compression           => $kombu_compression,
     }
+    oslo::messaging::amqp { 'swift_proxy_config': }
+
   } elsif $default_transport_url =~ /^amqp.*/ {
-    oslo::messaging::amqp {'swift_proxy_config':
+    oslo::messaging::rabbit { 'swift_proxy_config': }
+    oslo::messaging::amqp { 'swift_proxy_config':
       ssl_ca_file      => $notification_ssl_ca_file,
       ssl_cert_file    => $notification_ssl_cert_file,
       ssl_key_file     => $notification_ssl_key_file,

releasenotes/notes/oslo-messaging-rabbit-opts-e4da4a73d1f2c788.yaml
@@ -1,0 +1,15 @@
+---
+features:
+  - |
+    The ``swift::proxy::ceilometer`` class now supports the following new
+    parameters to manage options for Rabbit driver in oslo.messaging library.
+
+    - ``rabbit_ha_queues``
+    - ``heartbeat_timeout_threshold``
+    - ``heartbeat_rate``
+    - ``heartbeat_in_pthread``
+    - ``rabbit_qos_prefetch_count``
+    - ``amqp_durable_queues``
+    - ``kombu_reconnect_delay``
+    - ``kombu_failover_strategy``
+    - ``kombu_compression``

spec/classes/swift_proxy_ceilometer_spec.rb
@@ -78,12 +78,22 @@ describe 'swift::proxy::ceilometer' do
       end
 
       it { is_expected.to contain_oslo__messaging__rabbit('swift_proxy_config').with(
-        :rabbit_use_ssl     => '<SERVICE DEFAULT>',
-        :kombu_ssl_ca_certs => '<SERVICE DEFAULT>',
-        :kombu_ssl_certfile => '<SERVICE DEFAULT>',
-        :kombu_ssl_keyfile  => '<SERVICE DEFAULT>',
-        :kombu_ssl_version  => '<SERVICE DEFAULT>',
+        :rabbit_ha_queues            => '<SERVICE DEFAULT>',
+        :heartbeat_timeout_threshold => '<SERVICE DEFAULT>',
+        :heartbeat_rate              => '<SERVICE DEFAULT>',
+        :heartbeat_in_pthread        => '<SERVICE DEFAULT>',
+        :rabbit_qos_prefetch_count   => '<SERVICE DEFAULT>',
+        :amqp_durable_queues         => '<SERVICE DEFAULT>',
+        :kombu_ssl_ca_certs          => '<SERVICE DEFAULT>',
+        :kombu_ssl_certfile          => '<SERVICE DEFAULT>',
+        :kombu_ssl_keyfile           => '<SERVICE DEFAULT>',
+        :kombu_ssl_version           => '<SERVICE DEFAULT>',
+        :rabbit_use_ssl              => '<SERVICE DEFAULT>',
+        :kombu_reconnect_delay       => '<SERVICE DEFAULT>',
+        :kombu_failover_strategy     => '<SERVICE DEFAULT>',
+        :kombu_compression           => '<SERVICE DEFAULT>',
       )}
+      it { is_expected.to contain_oslo__messaging__amqp('swift_proxy_config') }
 
       context 'with overridden rabbit ssl params' do
         before do