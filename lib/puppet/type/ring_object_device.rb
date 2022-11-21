4b37f4c4df42c3069b502ee9f539fdb5d0d4c897
Takashi Kajinami <tkajinam@redhat.com>
2022-11-21 13:55:47 +0900

    Ensure consistent IPv6 address format
    This change ensures the shortened address representations are always
    used for ring devices to avoid broken idempotency caused by different
    representations (shortened[1] vs canonical[2]).
    
    [1] ::1
    [2] 0000:0000:0000:0000:0000:0000:0000:0001
    
    Closes-Bug: #1997313
    Change-Id: I762f0780ba25536aa616482a49d59c7db69924d6
    (cherry picked from commit 249f466f94441ae554478467949378d81b034f89)

--- lib/puppet/type/ring_object_device.rb
+++ lib/puppet/type/ring_object_device.rb
@@ -14,11 +14,28 @@ Puppet::Type.newtype(:ring_object_device) do
       # we have to have URI Scheme so we just add http:// and ignore it later
       uri = URI('http://' + value)
       address = uri.host
-      port_device = uri.port
       if ['','/'].include?(uri.path)
         raise(Puppet::Error, "namevar should contain a device")
       end
       IPAddr.new(address)
+    end
+
+    munge do |value|
+      # If storage policy_index is specified first strip that off.
+      # Resource name is not required to start with a policy_index
+      if !value.split(/^\d+:/)[1].nil?
+        value_without_policy = value.split(/^\d+:/)[1]
+      else
+        value_without_policy = value
+      end
+      # we have to have URI Scheme so we just add http:// and ignore it later
+      uri = URI('http://' + value_without_policy)
+      ip = IPAddr.new(uri.host)
+      if ip.ipv6?
+        value.gsub(uri.host, '[' + ip.to_s + ']')
+      else
+        value
+      end
     end
   end
 
