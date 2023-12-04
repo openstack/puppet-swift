4ae639589e7a2033bd39b0df7750e08dde5322a6
Takashi Kajinami <kajinamit@oss.nttdata.com>
2023-12-04 15:20:49 +0900

    Fix incomplete unit tests for swift::containerreconciler
    This fixes some of the logics which do not actually enable unit tests,
    to keep appropriate test coverage.
    
    Change-Id: I27ffddfe37d6801df7b4b2caf7becd7d92773ddc

--- spec/classes/swift_containerreconciler_spec.rb
+++ spec/classes/swift_containerreconciler_spec.rb
@@ -2,19 +2,6 @@ require 'spec_helper'
 
 describe 'swift::containerreconciler' do
 
-  let :default_params do
-    { :manage_service    => true,
-      :enabled           => true,
-      :package_ensure    => 'present',
-      :pipeline          => ['catch_errors', 'proxy-logging', 'proxy-server'],
-      :interval          => '<SERVICE DEFAULT>',
-      :reclaim_age       => '<SERVICE DEFAULT>',
-      :request_tries     => '<SERVICE DEFAULT>',
-      :memcache_servers  => ['127.0.0.1:11211'],
-      :cache_tls_enabled => false,
-    }
-  end
-
   let :params do
     {}
   end
@@ -24,26 +11,34 @@ describe 'swift::containerreconciler' do
   end
 
   shared_examples 'swift::container::reconciler' do
-    let (:p) { default_params.merge!(params) }
-
     context 'with defaults' do
       it 'configures container-reconciler.conf' do
         is_expected.to contain_swift_container_reconciler_config(
-          'pipeline:main/pipeline').with_value(p[:pipeline].join(' '))
+          'pipeline:main/pipeline').with_value('catch_errors proxy-logging proxy-server')
         is_expected.to contain_swift_container_reconciler_config(
-          'container-reconciler/interval').with_value(p[:interval])
+          'container-reconciler/interval').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_swift_container_reconciler_config(
-          'container-reconciler/reclaim_age').with_value(p[:reclaim_age])
+          'container-reconciler/reclaim_age').with_value('<SERVICE DEFAULT>')
         is_expected.to contain_swift_container_reconciler_config(
-          'container-reconciler/request_tries').with_value(p[:request_tries])
+          'container-reconciler/request_tries').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_swift_container_reconciler_config(
+          'filter:cache/memcache_servers').with_ensure('absent')
+        is_expected.to contain_swift_container_reconciler_config(
+          'filter:cache/tls_enabled').with_ensure('absent')
+        is_expected.to contain_swift_container_reconciler_config(
+          'filter:cache/tls_cafile').with_ensure('absent')
+        is_expected.to contain_swift_container_reconciler_config(
+          'filter:cache/tls_certfile').with_ensure('absent')
+        is_expected.to contain_swift_container_reconciler_config(
+          'filter:cache/tls_keyfile').with_ensure('absent')
       end
 
       it 'configures container-reconciler service' do
         is_expected.to contain_service('swift-container-reconciler').with(
-          :ensure   => (p[:manage_service] && p[:enabled]) ? 'running' : 'stopped',
+          :ensure   => 'running',
           :name     => platform_params[:service_name],
           :provider => platform_params[:service_provider],
-          :enable   => p[:enabled]
+          :enable   => true,
         )
       end
     end
@@ -55,6 +50,15 @@ describe 'swift::containerreconciler' do
           :reclaim_age   => '10000',
           :request_tries => '3',
         )
+      end
+
+      it 'configures container-reconciler.conf' do
+        is_expected.to contain_swift_container_reconciler_config(
+          'container-reconciler/interval').with_value(600)
+        is_expected.to contain_swift_container_reconciler_config(
+          'container-reconciler/reclaim_age').with_value(10000)
+        is_expected.to contain_swift_container_reconciler_config(
+          'container-reconciler/request_tries').with_value(3)
       end
     end
 
@@ -68,20 +72,19 @@ describe 'swift::containerreconciler' do
 
       it 'configures memcache servers' do
         is_expected.to contain_swift_container_reconciler_config(
-          'filter:cache/memcache_servers').with_value(p[:memcache_servers])
+          'pipeline:main/pipeline').with_value('catch_errors proxy-logging cache proxy-server')
+        is_expected.to contain_swift_container_reconciler_config(
+          'filter:cache/memcache_servers').with_value('127.0.0.1:11211')
+        is_expected.to contain_swift_container_reconciler_config(
+          'filter:cache/tls_enabled').with_value(false)
+        is_expected.to contain_swift_container_reconciler_config(
+          'filter:cache/tls_cafile').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_swift_container_reconciler_config(
+          'filter:cache/tls_certfile').with_value('<SERVICE DEFAULT>')
+        is_expected.to contain_swift_container_reconciler_config(
+          'filter:cache/tls_keyfile').with_value('<SERVICE DEFAULT>')
       end
     end
-
-    context 'when using swiftinit service provider' do
-      before do
-        params.merge!({ :service_provider => 'swiftinit' })
-      end
-
-      before do
-        platform_params.merge!({ :service_provider => 'swiftinit' })
-      end
-    end
-
   end
 
   on_supported_os({
@@ -95,11 +98,9 @@ describe 'swift::containerreconciler' do
       let(:platform_params) do
         case facts[:os]['family']
         when 'Debian'
-          { :service_name     => 'swift-container-reconciler',
-            :service_provider => nil }
+          { :service_name => 'swift-container-reconciler' }
         when 'RedHat'
-          { :service_name     => 'openstack-swift-container-reconciler',
-            :service_provider => nil }
+          { :service_name => 'openstack-swift-container-reconciler' }
         end
       end
 
