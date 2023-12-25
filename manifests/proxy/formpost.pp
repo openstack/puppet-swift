385c66d388370ec9b8a443a0130bb4157ccc4794
Takashi Kajinami <kajinamit@oss.nttdata.com>
2023-12-25 17:16:19 +0900

    Support allowed_digests options
    ... which were added as part of deprecation of sha1 signatures[1].
    
    [1] 2d063cd61f6915579840a41ac0248a26085e0245
    
    Change-Id: I8a27cebecbf1c47b2aeb8c1f1f00b1f1c90011d1

--- manifests/proxy/formpost.pp
+++ manifests/proxy/formpost.pp
@@ -1,6 +1,12 @@
 #
 # Configure swift formpost.
 #
+# == Parameters
+#
+#  [*allowed_digests*]
+#    The digest algorithm(s) supported for generating signatures.
+#    Optional. Defaults to $facts['os_service_default'].
+#
 # == Dependencies
 #
 # == Examples
@@ -15,11 +21,14 @@
 #
 # Copyright 2012 eNovance licensing@enovance.com
 #
-class swift::proxy::formpost() {
+class swift::proxy::formpost(
+  $allowed_digests = $facts['os_service_default'],
+) {
 
   include swift::deps
 
   swift_proxy_config {
-    'filter:formpost/use': value => 'egg:swift#formpost';
+    'filter:formpost/use':             value => 'egg:swift#formpost';
+    'filter:formpost/allowed_digests': value => join(any2array($allowed_digests), ' ');
   }
 }
