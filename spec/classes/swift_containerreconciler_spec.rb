e4941136f1eaa98085c0bb606478b0169066ec41
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-10-26 18:18:26 +0900

    Support more log_address options
    Support customizing log_address options for object-expirer and
    container-reconciler for better parameter coverage.
    
    Change-Id: I60a2fef439e2e70019d906bab33c8ea7ebe9098e

--- spec/classes/swift_containerreconciler_spec.rb
+++ spec/classes/swift_containerreconciler_spec.rb
@@ -41,6 +41,8 @@ describe 'swift::containerreconciler' do
         is_expected.to contain_swift_container_reconciler_config(
           'container-reconciler/log_facility').with_value('LOG_LOCAL2')
         is_expected.to contain_swift_container_reconciler_config(
+          'container-reconciler/log_address').with_value('/dev/log')
+        is_expected.to contain_swift_container_reconciler_config(
           'filter:cache/memcache_servers').with_value('127.0.0.1:11211')
         is_expected.to contain_swift_container_reconciler_config(
           'filter:cache/tls_enabled').with_value('<SERVICE DEFAULT>')
